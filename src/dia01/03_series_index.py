ba1327e39d0e2d82089ca4fb13b215693bc3f612
Gustavo Guimarães <gustavojobwork@gmail.com>
2025-07-11 00:06:48 -0300

    file: mudando arquivo 3

--- src/dia01/03_series_index.py
+++ src/dia01/03_series_index.py
@@ -1,4 +1,4 @@
-# %%
+# %% 
 import pandas as pd
 
 idades = [
@@ -6,18 +6,14 @@ idades = [
     35, 25, 29, 31, 37,
     27, 23, 36, 33, 39
 ]
-
-series_idades = pd.Series(idades)
-
-# %%
-# iloc puxa a posição na série ao invés de puxar o indice
-series_idades.iloc[0]
-# %%
 indexs = [
 "Téo", "Maria", "Jose", "Luis", "Ana",
 "Nah", "Dani", "Mah", "Fer", "Nanda",
 "Naty", "Nih", "Pedro", "Kozato", "Kozato"
 ]
+series_idades = pd.Series(idades)
 
+# iloc consulta a posição na série ao invés do indíce
+series_idades.iloc[0]
+# alterando o indíce padrão para a lista indexs
 series_idades = pd.Series(idades, index=indexs)
-series_idades["Kozato"]
